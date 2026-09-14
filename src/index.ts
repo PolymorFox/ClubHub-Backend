4cb31154111b07ef6acaa7c121ef5af3ed547924
PolymorFox <drmagnet1234@protonmail.com>
2026-09-14 19:16:15 +0000

    deps: add cors with allowed origins specified via .env file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import express from "express";
 import { diagnosticRoutes } from "./routes/diagnosticRoutes.js";
 import { userRoutes } from "./routes/userRoutes.js";
+import dotenv from "dotenv"
+import cors from 'cors'
+
+// Load env variables from .env file
+dotenv.config()
 
 const app = express();
 app.use(express.json());
+
+// Use cors only accepts request from vite dev server, or from frontend dev server
+const corsOptions: object = {
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}
+
+app.use(cors(corsOptions))
 
 // Mount routes for a diagnostic checks on root path
 app.use("/", diagnosticRoutes);
